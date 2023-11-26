e0b481a232cf9566e4cbef20c22107e2246aca0c
Vidyesh Churi <vidyesh95@gmail.com>
2023-11-26 21:56:48 +0530

    Update ContentView.swift
    Completed beginner ios app.

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -2,20 +2,35 @@
 //  ContentView.swift
 //  EmojiLover
 //
-//  Created by Vinit Bipin Churi on 26/11/23.
+//  Created by Vidyesh Bipin Churi on 26/11/23.
 //
 
 import SwiftUI
 
-enum Emoji: String {
-    case 🥰,🤟🏻,🎾,🍀,🌷
+enum Emoji: String, CaseIterable {
+    case smilingFaceWithHeart="🥰"
+    case iLoveYou="🤟🏻"
+    case tennisRacketAndBall="🎾"
+    case fourLeafClover="🍀"
+    case tulip="🌷"
 }
 
 struct ContentView: View {
+    @State var selection: Emoji = Emoji.tulip
+    
     var body: some View {
-        var selection: Emoji = .🌷
-        Text("Hello"+selection.rawValue)
-            .font(.system(size: 32))
+        NavigationView {
+            VStack {
+                Text("Hello"+selection.rawValue)
+                    .font(.system(size: 32))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+            }.navigationTitle("Emoji Lovers!").padding()
+        }
     }
 }
 
